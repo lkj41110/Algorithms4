615c58ceb2f255c05332810ca89c06c5787f6c71
luokai <807637152@qq.com>
2020-01-01 22:15:50 +0800

    符号表（map ）

---
+++ src/main/java/map/ST.java
@@ -0,0 +1,56 @@
+package map;
+
+
+/**
+ * @author luokai
+ * @description:
+ * @date: 2020-01-01
+ * @version: 1.0
+ */
+public interface ST<K, V> {
+    /**
+     * 放入队列
+     *
+     * @param key
+     * @param value
+     */
+    void put(K key, V value);
+
+    /**
+     * 获取队列
+     *
+     * @param key
+     * @return
+     */
+    V get(K key);
+
+    /**
+     * 删除
+     *
+     * @param key
+     */
+    void delete(K key);
+
+    /**
+     * 是否包含
+     *
+     * @param key
+     * @return
+     */
+    boolean contains(K key);
+
+    /**
+     * 是否包含
+     *
+     * @param key
+     * @return
+     */
+    boolean isEmpty(K key);
+
+    /**
+     * 获取有多少数量
+     *
+     * @return
+     */
+    int size();
+}
