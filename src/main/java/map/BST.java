6bca43ba33dee61637838d2e612448aa87365519
luokai <807637152@qq.com>
2020-01-03 00:21:58 +0800

    二叉查找树 put main

--- src/main/java/map/BST.java
+++ src/main/java/map/BST.java
@@ -9,7 +9,10 @@ package map;
  * @version: 1.0
  */
 public class BST<Key extends Comparable, Value> implements ST<Key, Value> {
+
     private Node root;
+
+    private int max = 0;
 
     private class Node {
         private Key key;
@@ -20,25 +23,47 @@ public class BST<Key extends Comparable, Value> implements ST<Key, Value> {
          * 以当前节点为root的总节点数
          */
         private int n;
+        /**
+         * 层高
+         */
+        private int high;
 
-        public Node(Key key, Value value, int n) {
+        public Node(Key key, Value value, int n, int high) {
             this.key = key;
             this.value = value;
             this.n = n;
+            this.high = high;
         }
     }
 
     @Override
     public void put(Key key, Value value) {
-        put(key, value, root);
+        root = put(key, value, root, 1);
     }
 
-    private void put(Key key, Value value, Node node) {
-        //if (node == null) {
-        //    node=new Node()
-        //}
+    private Node put(Key key, Value value, Node node, int high) {
+        if (node == null) {
+            Node temp = new Node(key, value, 1, high + 1);
+            if (temp.high > max) {
+                max = temp.high;
+            }
+            return temp;
+        }
+        int cmd = key.compareTo(node.key);
+        if (cmd > 0) {
+            node.left = put(key, value, node.left, high + 1);
+        } else if (cmd < 0) {
+            node.right = put(key, value, node.right, high + 1);
+        } else {
+            node.value = value;
+        }
+        node.n = size(node.right) + size(node.left) + 1;
+        return node;
     }
 
+    /**
+     * 递归实现版本
+     */
     @Override
     public Value get(Key key) {
         return get(key, root);
@@ -74,9 +99,30 @@ public class BST<Key extends Comparable, Value> implements ST<Key, Value> {
 
     @Override
     public int size() {
-        if (root == null) {
+        return size(root);
+    }
+
+    private int size(Node node) {
+        if (node == null) {
             return 0;
         }
-        return root.n;
+        return node.n;
     }
+
+    @Override
+    public String toString() {
+        //TODO 中序遍历 + 格式化
+        return null;
+    }
+
+    public static void main(String[] args) {
+        BST bst = new BST();
+        bst.put(1, "A1");
+        bst.put(5, "A5");
+        bst.put(9, "A9");
+        bst.put(7, "A7");
+
+        System.out.println(bst);
+    }
+
 }
