7c24376facd0ac5d409b30e336397325604320e3
luokai <807637152@qq.com>
2020-01-31 21:11:28 +0800

    leetCode94 先序遍历

--- src/main/java/leetcode/tree/Solution94.java
+++ src/main/java/leetcode/tree/Solution94.java
@@ -1,68 +1,102 @@
 package leetcode.tree;
 
+
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author luokai
  * @description:
  * @date: 2019/2/28
  * @version: 1.0
- *
- *
+ * <p>
+ * <p>
  * 中序遍历 （非递归算法）
- *
+ * <p>
  * 输入: [1,null,2,3]
- *    1
- *     \
- *      2
- *     /
- *    3
- *
- *
- *
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * <p>
+ * <p>
  * 输出: [1,3,2]
  */
 public class Solution94 {
+    class Pair<T, I> {
+        private T key;
+        private I value;
+
+        public Pair(T key, I value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public T getKey() {
+            return key;
+        }
+
+        public void setKey(T key) {
+            this.key = key;
+        }
+
+        public I getValue() {
+            return value;
+        }
+
+        public void setValue(I value) {
+            this.value = value;
+        }
+    }
+
+    /**
+     * 迭代算法1
+     * 首先想到的，有点想复杂了
+     *
+     * @param root
+     * @return
+     */
     public List<Integer> inorderTraversal(TreeNode root) {
+
         List<Integer> r = new ArrayList<>();
-        LinkedList<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        TreeNode node = root;
-        //do {
-        //    //
-        //    while (node.left != null) {
-        //        queue.addLast(node.left);
-        //        node = node.left;
-        //    }
-        //    //打印
-        //    node = queue.pollLast();
-        //    r.add(node.val);
-        //    if (node.right != null) {
-        //        node = node.right;
-        //    } else {
-        //        node = queue.pollLast();
-        //        r.add(node.val);
-        //    }
-        //} while (node!=null);
+        if(root==null){
+            return r;
+        }
+        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
 
+        stack.add(new Pair<>(root, 1));
+        while (stack.size() > 0) {
+            Pair<TreeNode, Integer> parent = stack.peek();
+            TreeNode pNode = parent.getKey();
+            Integer value = parent.getValue();
 
-        while (node != null || !queue.isEmpty()) {
-            while (node != null) {
-                queue.push(node);
-                node = node.left;
+            TreeNode cNode = null;
+            //左子树没有遍历过
+            if (value == 1) {
+                cNode = pNode.left;
+            } else if (value == 0) {
+                r.add(pNode.val);
+                cNode = pNode.right;
             }
-            node = queue.pop();
-            r.add(node.val);
-            node = node.right;
+
+            stack.pop();
+            if (value >= 1) {
+                stack.push(new Pair(pNode, value - 1));
+            }
+            if (cNode != null) {
+                stack.add(new Pair<>(cNode, 1));
+            }
+
         }
         return r;
     }
 
     public static void main(String[] args) {
         Solution94 solution = new Solution94();
-        List<Integer> integers = solution.inorderTraversal(new TreeNode(new int[]{1,2, 3, -1,4,5,6}));
-        //solution.constructMaximumBinaryTree(new int[]{3, 2, 1, 6, 0, 5});
+        List<Integer> integers = solution.inorderTraversal(new TreeNode(new int[]{}));
+        System.out.println(integers);
     }
 }
