963e47e4da67af47f7ce0a012586462139b0b5bb
luokai <807637152@qq.com>
2020-03-09 23:07:04 +0800

    分割回文串

--- src/main/java/leetcode/test/Solution2.java
+++ src/main/java/leetcode/test/Solution2.java
@@ -1,68 +1,48 @@
 package leetcode.test;
 
 import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
  * 分割回文串
  */
 class Solution2 {
-    public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<>();
-
-        if (s.length() == 0) {
+    public List<String> letterCombinations(String digits) {
+        char a[][] = {{}, {'a', 'b', 'c'},
+                {'d', 'e', 'f'},
+                {'g', 'h', 'i'},
+                {'j', 'k', 'l'},
+                {'m', 'n', 'o'},
+                {'p', 'q', 'r', 's'},
+                {'t', 'u', 'v'},
+                {'w', 'x', 'y', 'z'}
+        };
+        List<String> res = new ArrayList<>();
+        String str = "";
+        if (digits.length() == 0) {
             return res;
         }
-        Deque<String> temp = new LinkedList<>();
-        doPartition(s, 0, res, temp);
-
+        doLetterCombinations(digits, 0, a, res, str);
         return res;
-
     }
 
-    private void doPartition(String s, int start, List res, Deque temp) {
-        if (start == s.length()) {
-            res.add(new ArrayList<>(temp));
+    private static void doLetterCombinations(String str, int start, char a[][], List<String> res, String temp) {
+        if (start == str.length()) {
+            res.add(temp);
             return;
         }
-
-        for (int i = start; i < s.length(); i++) {
-            if (!checkPalindrome(s, start,  i)) {
-                continue;
-            }
-            temp.addLast(s.substring(start,  i + 1));
-            doPartition(s,  i + 1, res, temp);
-            temp.removeLast();
-
+        int num = str.charAt(start) - '0';
+        for (int i = 0; i < a[num].length; i++) {
+            temp = temp + a[num][i];
+            doLetterCombinations(str, start + 1, a, res, temp);
+            temp = temp.substring(0, temp.length() - 1);
         }
-    }
-
-    /**
-     * 判断是否是回文
-     *
-     * @param str
-     * @param left
-     * @param right
-     * @return
-     */
-    private boolean checkPalindrome(String str, int left, int right) {
-        // 严格小于即可
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
     }
 
 
     public static void main(String[] args) {
         Solution2 a = new Solution2();
-        List<List<String>> palindrome = a.partition("aab");
+        List<String> palindrome = a.letterCombinations("23");
         System.out.println(palindrome);
     }
 }
