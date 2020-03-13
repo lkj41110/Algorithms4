6a0db41c992fde373119044c56c92c3107269cc3
luokai <807637152@qq.com>
2020-03-13 23:31:37 +0800

    投票问题 修改

--- src/main/java/leetcode/other/Solution229.java
+++ src/main/java/leetcode/other/Solution229.java
@@ -40,11 +40,22 @@ public class Solution229 {
                 }
             }
         }
+        //检查两个票数符不符合
+        num1 = num2 = 0;
+        for (int num : nums) {
+            if (num == temp1) {
+                num1++;
+            } else if (num == temp2) {
+                //这里一定要用else if，因为可能出现[0,0,0]这种用例，导致两个cand是一样的，写两个if结果就变为[0,0]了
+                num2++;
+            }
+        }
+
         List<Integer> list = new ArrayList<>();
-        if(num1!=0){
+        if(num1>nums.length/3){
             list.add(temp1);
         }
-        if(num2!=0){
+        if(num2>nums.length/3){
             list.add(temp2);
         }
         return list;
