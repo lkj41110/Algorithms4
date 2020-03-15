9853f394a90fa6a25afc32fe5eb28f4cea96ce50
luokai <807637152@qq.com>
2020-03-15 11:51:16 +0800

    二分查找

--- src/main/java/leetcode/erfen/Solution.java
+++ src/main/java/leetcode/erfen/Solution.java
@@ -1,24 +1,16 @@
 package leetcode.erfen;
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode(int x) { val = x; }
- * }
- */
 public class Solution {
 
     /**
-     * 二分查找
+     * 二分查找最普通的
      *
      * @param nums
      */
-    public int test(int nums[], int num) {
+    public int test1(int nums[], int num) {
         int s = 0, e = nums.length - 1;
         while (s <= e) {
-            int mid = (e + s) / 2;
+            int mid = s + (e - s) / 2;
             System.out.println("中间数  " + mid);
             if (num > nums[mid]) {
                 s = mid + 1;
@@ -28,12 +20,35 @@ public class Solution {
                 return mid;
             }
         }
-        return s;
+        return -1;
+    }
+
+    /**
+     * 二分查
+     * 第一个相似的
+     *
+     * @param nums
+     */
+    public int test2(int nums[], int num) {
+        int s = 0, e = nums.length - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            System.out.println("中间数  " + mid);
+            if (num > nums[mid]) {
+                s = mid + 1;
+            } else if (num <= nums[mid]) {
+                e = mid - 1;
+            }
+        }
+        if (s < nums.length && nums[s] == num) {
+            return s;
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
-        int a[] = {2, 7, 10, 21, 168, 172, 200};
+        int a[] = {2, 7, 7, 7, 7, 7, 7, 10, 21, 168, 172, 200};
         Solution solution = new Solution();
-        System.out.println("最终位置 " + solution.test(a, 9));
+        System.out.println("最终位置 " + solution.test2(a, 7));
     }
 }
