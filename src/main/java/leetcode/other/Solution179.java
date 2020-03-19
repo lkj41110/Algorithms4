8f7c4e91402434c1a2f6b4dd139ef2bbcc35c909
luokai <807637152@qq.com>
2020-03-19 22:51:56 +0800

    方法二

---
+++ src/main/java/leetcode/other/Solution179.java
@@ -0,0 +1,26 @@
+package leetcode.other;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * 快排 比较方法修改
+ */
+public class Solution179 {
+
+
+
+    public static void main(String[] args) {
+        Solution179 solution = new Solution179();
+        //int[] a = {0, 0, 0};
+        int[] a = {0, 2, 2, 3, 0, 1, 2, 3, -1, -4, 2};
+        System.out.println(solution.threeSum(a));
+        //int[] b = {3, 2, 3, 1, 2, 4, 5, 5, 6};
+        //System.out.println(solution.threeSum(b, 4));
+    }
+
+
+}
