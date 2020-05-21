68b1daedf66e56fe98350eca9bc03c121d4e17dc
luokai <807637152@qq.com>
2020-05-21 23:55:21 +0800

    递归排序

--- src/main/java/leetcode/tree/Solution1448.java
+++ src/main/java/leetcode/tree/Solution1448.java
@@ -8,22 +8,44 @@ package leetcode.tree;
  */
 public class Solution1448 {
 
-    int sum = 0;
+    public int findTheLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
 
-    public int goodNodes(TreeNode root) {
-        dfs(root, Integer.MIN_VALUE);
-        return sum;
+        int[] pos = new int[32];
+        for (int i = 0; i < 32; i++) {
+            pos[i] = -1;
+        }
+
+        int status = 0;
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == 'a') {
+                status = status ^ 1;
+            } else if (ch == 'e') {
+                status = status ^ 1 << 1;
+            } else if (ch == 'i') {
+                status = status ^ 1 << 2;
+            } else if (ch == 'o') {
+                status = status ^ 1 << 3;
+            } else if (ch == 'u') {
+                status = status ^ 1 << 4;
+            }
+
+            if (pos[status] == -1) {
+                pos[status] = i;
+            } else {
+                max = Math.max(max, i - pos[status] + 1);
+            }
+        }
+        return max;
     }
 
-    public void dfs(TreeNode root, int max) {
-        if (root == null) {
-            return;
-        }
-        if (max < root.val) {
-            sum++;
-        }
-        dfs(root.right, root.val > max ? root.val : max);
-        dfs(root.left, root.val > max ? root.val : max);
-
+    public static void main(String[] args) {
+        System.out.println(1^1);
+        System.out.println(1^0);
+        System.out.println(0^0);
     }
 }
