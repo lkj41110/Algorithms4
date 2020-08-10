c578a14f80a8176ad7aca628f609a55ae5ae1d0e
luokai <807637152@qq.com>
2020-08-10 23:59:49 +0800

    dfs

--- src/main/java/leetcode/other/Solution63.java
+++ src/main/java/leetcode/other/Solution63.java
@@ -10,7 +10,7 @@ public class Solution63 {
             return 0;
         }
         int res = 0;
-        for (int i = 1; i < s.length() ; i++) {
+        for (int i = 1; i < s.length(); i++) {
             int len = 1;
             char a = '0';
             while (i + len - 1 < s.length() && i - len >= 0) {
@@ -20,7 +20,7 @@ public class Solution63 {
                         res++;
                     } else if (a == s.charAt(i + len - 1)) {
                         res++;
-                    }else{
+                    } else {
                         break;
                     }
                     len++;
@@ -32,10 +32,43 @@ public class Solution63 {
         return res;
     }
 
+    //方法1，笨重
+    public int countBinarySubstrings1(String s) {
+        if (s.length() == 0 || s.length() == 1) {
+            return 0;
+        }
+        int now = 0;
+        int la = 0;
+        char la_ch = 'a';
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (now == 0) {
+                now++;
+                la_ch = ch;
+            } else if (ch == la_ch) {
+                now++;
+            } else {
+                if (la == 0) {
+                    la = now;
+                } else {
+                    res += Math.min(la, now);
+                }
+                now = 1;
+                la_ch = ch;
+            }
+        }
+        if (la != 0) {
+            res += Math.min(la, now);
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         Solution63 so = new Solution63();
         int[][] a = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-        int i = so.countBinarySubstrings("10101");
+        int i = so.countBinarySubstrings1("00110011");
         System.out.println(i);
     }
 }
